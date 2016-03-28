d20376e02ef6e4b5f99661883607b5dc0eb39b9b
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2016-03-28 00:54:31 +0000

    Initial commit

---
+++ config/default.js
@@ -0,0 +1,31 @@
+'use strict';
+var uuid = require('node-uuid');
+
+module.exports = {
+    execMap: {
+        py: 'python',
+        rb: 'ruby'
+    },
+    session: {
+        secret: uuid.v1(),
+        resave: false,
+        saveUninitialized: false
+    },
+    websocket: {
+        port: 13337
+    },
+    http: {
+        port: process.env.PORT || 8111
+    },
+    https: {
+        port: 8443
+    },
+    log: {
+        level: 'error'
+    },
+    csp: {
+        'default-src': '\'self\'',
+        'script-src':  '\'self\' cdnjs.cloudflare.com',
+        'font-src': '\'self\' fonts.gstatic.com data:'
+    }
+};
